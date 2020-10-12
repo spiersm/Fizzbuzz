330080b117fece643a5a41c366d0973844670878
spiersm <spiersme@missouri.edu>
2020-10-12 15:00:23 -0500

    Adding first version of Fizzbuzz.py

---
+++ Fizzbuzz.py
@@ -0,0 +1,11 @@
+\\for x in range(1, 101):
+
+for(x==1; x<=100; x++):
+    if x%3 == 0 and x%5 == 0:
+        print("FizzBuzz")
+    elif x%3 == 0:
+        print("Fizz")
+    elif x%5 == 0:
+        print("Buzz")
+    else:
+        print(x)
